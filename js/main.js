0d02b30bcc0264a8f8b85d64685d4695e60a4fc9
Zach Alcerro <zachalcerro@gmail.com>
2019-07-11 21:53:23 +0900

    adds a note

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 
+//initialize magnific popup
 $('.flex-container').magnificPopup({
     delegate: 'a', // child items selector, by clicking on it popup will open
     type: 'image',
     gallery: {
-        // options for gallery
         enabled: true,
         navigateByImgClick: true,
       },
